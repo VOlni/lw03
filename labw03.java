69339198115138d90a71d2349014e1d46a4510cb
VOlni <salivan13@gmail.com>
2016-10-25 13:18:31 +0300

    Initial commit

---
+++ labw03.java
@@ -0,0 +1,50 @@
+public class labw03 implements var04 {
+	
+	public double logika(double d1, double d2, double d3); {
+		// Даны три числа. Найти количество положительных чисел в исходном наборе.
+		
+		int countPositive = 0;
+		
+		if (d1 > 0) {
+			countPositive++;
+		}
+		if (d2 > 0) {
+			countPositive++;
+		}
+		if (d2 > 0) {
+			countPositive++;
+		}
+		
+		System.out.println("Количество положительных чисел в исходном наборе: " + countPositive);
+		return 0;
+	}
+  
+	public void poka(int N); {
+		// Дано целое число N (> 0). Используя операции деления нацело и
+		// взятия остатка от деления, вывести все его цифры, начиная с 
+		// самой правой (разряда единиц).
+		do {
+			System.out.println(N % 10);
+			N = N / 10;
+		} while (N > 0);
+	
+	}
+ 
+	public void massivy(double[] A, double D); {
+		// Найти элемент массива A, модуль разницы которого с D минимален
+		double[] B;
+		
+		for (int i = 0; i < A.length ; i++){
+			B[i] = Math.abs(A[i] - D);
+		}
+		
+		double min = A[0];
+		for(int i = 0; i < B.length; i++){
+            if(min > B[i])
+                min = B[i];
+        }
+		System.out.println("Минимальный элемент массива №: " + min);
+		
+	}
+  	
+}
---
+++ vars/var06.java
@@ -0,0 +1,6 @@
+interface var06 {
+
+  public double logika(double d1, double d2, double d3);
+  // Даны три числа. Выдать порядковый номер меньшего из них. 
+
+}
